6fb92ae48c74f7ad14665c90e1913cce3cff6541
pulabelitaa <johnforelli4@gmail.com>
2024-11-02 18:15:04 +0000

    fixed astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-git add . 
+  site: 'https://b8e473bf.astro-ade.pages.dev/',
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
