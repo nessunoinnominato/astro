d73052be0cc17849c97c221b1665bca34ad8fc70
serega <johnforelli4@gmail.com>
2025-05-16 13:45:10 +0200

    fixed preline init in main.js and updated import on layout.astro

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,12 +1,7 @@
-// Preline library import
-document.addEventListener("DOMContentLoaded", () => {
-  import("preline/preline");
-});
-
 window.addEventListener("DOMContentLoaded", () => {
   if (window.feather) {
     window.feather.replace();
   }
 });
 
-console.log("everything works yay");
+console.log("Feather loaded");
